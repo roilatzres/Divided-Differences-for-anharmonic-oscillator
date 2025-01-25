d54b7e3779ffa4cfaddab14476f6add559e791bd
roilatzres <roilatzres@gmail.com>
2025-01-25 15:26:34 +0200

    final commit for sin pulse

--- main_orig.cpp
+++ main_orig.cpp
@@ -9,20 +9,20 @@
 #include <fstream>
 #include "include/json-develop/single_include/nlohmann/json.hpp"
 #include <string>
+// #include "main_orig.h"
 
 using std::vector;
 using std::complex;
 
 // #define  MAX_STEP 14
-#define  MAX_TARGET 6
-#define MAX_Q 13
+#define  MAX_TARGET 8
+#define MAX_Q 10
 
 
 
 int main(int argc, char* argv[]) {
     complex<double> i_num(0, 1);
-    //print i_num
-    std::cout << "i_num: " << i_num << std::endl;
+    
     //original parameters
     double multiplier = 4;
     float sigma = 48;
@@ -30,9 +30,11 @@ int main(int argc, char* argv[]) {
     double chi = -279e-6 * 2 * M_PI; 
     // double detuning = chi *1e-2;
 
+
+    //TODO: notice that amp is double of the value to put in antiSymSim
     // initial values
-    double amplitude = 0.001;
-    int q_max = 10; // TODO: try 11
+    double amplitude = 0.02;
+    int q_max = 12; 
     
     if(argc > 1){
         amplitude = std::stod(argv[1]);
@@ -41,8 +43,7 @@ int main(int argc, char* argv[]) {
 
     //print args
     std::cout << "Amplitude: " << amplitude << std::endl;
-    std::cout << "Q_max: " << q_max << std::endl;
-
+    std::cout << "q_max: " << q_max << std::endl;
 
     // //print vars
     // std::cout << "Multiplier: " << multiplier << std::endl;
@@ -69,7 +70,7 @@ int main(int argc, char* argv[]) {
     double t;
 
     // vector of all transition ampli tudes for all times: [time][target_state][amp]
-    vector<vector<complex<double>>> all_transition_amplitudes;
+    vector<vector<vector<complex<ExExFloat>>>> all_transition_amplitudes(2);
     vector<vector<complex<double>>> all_transition_amplitudes_g;
     vector<vector<complex<double>>> all_transition_amplitudes_e;
     
@@ -83,35 +84,7 @@ int main(int argc, char* argv[]) {
     vector< //permutations
     std::tuple<vector<DivdiffElement>, double, int, vector<int>>> // coefficients
     >> all_coefficients;
-
-    
-    // Loop over target steps and moves
-    for (int target_step = 0; target_step <= MAX_TARGET; ++target_step) {
-        all_coefficients.push_back(vector<vector<std::tuple<vector<DivdiffElement>, double, int, vector<int>>>>());
-
-        for (int q = 0; q <= MAX_Q; ++q) {
-            std::string filename = "./include/parms/dispersive/coeff/dispersive_coeff_" 
-                + std::to_string(target_step) + "_" + std::to_string(q) + ".bin";
-
-            // Check if file exists
-            std::ifstream infile(filename);
-            if (!infile) {
-                std::cout << "File " << filename << " does not exist." << std::endl;
-                // generate and save coefficients
-                generate_and_save_coefficients(start_state, total_steps, target_step, q, filename);
-
-            } else {
-
-                // Add a new vector for this q
-                all_coefficients[target_step].push_back(vector<std::tuple<vector<DivdiffElement>, double, int, vector<int>>>());
-                
-                // Load all_coefficients from file
-                vector<std::tuple<vector<DivdiffElement>, double, int, vector<int>>> value = load_coefficients(filename);
-                all_coefficients[target_step][q] = value;
-                                
-            }
-        }
-    }
+    load_all_coefficients(all_coefficients, start_state, total_steps, MAX_TARGET, q_max);
 
 
     // int qubit = 1;
@@ -119,35 +92,64 @@ int main(int argc, char* argv[]) {
 
         //loop over all times
         for (int i = 191; i < times.size(); i++) { //TODO: i=0
-            t = times[i];
-            // t = 192;
+            // t = times[i];
+            t = 192;
             
             //print time
             std::cout << std::endl << "Main-Time: " << t << std::endl;
 
             //vector of all transition amplitude for a given time
-            vector<complex<double>> transition_amplitudes;
+            vector<complex<ExExFloat>> transition_amplitudes;
             
             //loop over all target steps
             for (int target = 0; target <= MAX_TARGET; target++) {
-                complex<double> total_amp = 0;
+                complex<ExExFloat> total_amp;
+                total_amp *= 0;
+                // total_amp.imag() = 0;
+
+                //print total_amp
+                // std::cout << "Total amplitude: " << total_amp << std::endl << std::endl;
+                std::cout <<  "First Total amp: "; 
+                total_amp.real().print();
+                std::cout << "+ ";
+                total_amp.imag().print();
+                std::cout << "i" << std::endl; 
+                std::cout << std::endl;
 
                 //print target
                 std::cout << "Target: " << target << std::endl;
 
                 for(int q = 0; q <= q_max; q++){
                     //print q
-                    // std::cout << "Q: " << q << std::endl;
+                    std::cout << "Q: " << q << std::endl;
 
-                    complex<double> q_amp = 0; //TODO: change to EXEXFLOAT
+                    complex<ExExFloat> q_amp; //TODO: change to EXEXFLOAT
+                    q_amp *= 0;
+                    std::cout <<  "First q_amp: "; 
+                    total_amp.real().print();
+                    std::cout << "+ ";
+                    total_amp.imag().print();
+                    std::cout << "i" << std::endl; 
+                    std::cout << std::endl;
+
                     complex<double> energy_coefficient_amp = cal_neg_i_pow(q) * std::pow(amplitude, q);
 
                     if (target == 0 && q == 0){
-                        total_amp += 1;
+                        total_amp = 1;
+                        std::cout <<  "second Total amp: "; 
+
+                        total_amp.real().print();
+                        std::cout << "+ ";
+                        total_amp.imag().print();
+                        std::cout << "i" << std::endl; 
+                        std::cout << std::endl;
+
                         continue;
                     }
 
+                    // std::cout << "hi" << std::endl; 
                     auto coefficients = all_coefficients[target][q];
+                    // std::cout << "hi" << std::endl; 
 
                     // std::cout << "Permutations:" << std::endl;
                     // for (const auto& combination : permutations) {
@@ -163,52 +165,74 @@ int main(int argc, char* argv[]) {
                     }
 
                     
-                    // check if file for q_amp already exists
-                    std::string q_amp_filename = "./include/parms/dispersive/q_amp_recursive/dispersive_q_amp_qubit_" 
-                        + std::to_string(qubit) + "_t_" + std::to_string((int)t) + "_target_" + std::to_string(target) + "_q" + std::to_string(q) + ".bin";
-                    std::ifstream infile(q_amp_filename);
-                    if (infile) {//TODO: remove q != 12
-                        // std::cout << "File " << q_amp_filename << " already exists." << std::endl;
+                    // // check if file for q_amp already exists
+                    // std::string q_amp_filename = "./include/parms/dispersive/q_amp_recursive/dispersive_q_amp_qubit_" 
+                    //     + std::to_string(qubit) + "_t_" + std::to_string((int)t) + "_target_" + std::to_string(target) + "_q" + std::to_string(q) + ".bin";
+                    // std::ifstream infile(q_amp_filename);
+                    // if (infile) {//TODO: remove q != 12
+                    //     // std::cout << "File " << q_amp_filename << " already exists." << std::endl;
                         
-                        // load q_amp from file
-                        q_amp = load_complex_from_file(q_amp_filename);
+                    //     // load q_amp from file
+                    //     q_amp = load_complex_from_file(q_amp_filename);
 
-                        q_amp = q_amp * energy_coefficient_amp;
+                    //     q_amp = q_amp * energy_coefficient_amp;
                         
-                        // if(q > 12){ *************************************
-                            // print the q amplitude
-                            std::cout << q << " Original q_amp: " << q_amp << std::endl;
-                        // }***********************************************
-                        //add to total amp
-                        total_amp += q_amp; 
-                        continue;
-                    }
+                    //     // if(q > 12){ *************************************
+                    //         // print the q amplitude
+                    //         std::cout << q << " Original q_amp: " << q_amp << std::endl;
+                    //     // }***********************************************
+                    //     //add to total amp
+                    //     total_amp += q_amp; 
+                    //     continue;
+                    // }
 
-
+                    std::cout << "pre DIVDIFF" << std::endl;
                     // Need to calculate q_amp
                     divdiff_init();
+                    // std::cout << coefficients.size() << std::endl; 
                     for (const auto& coefficient : coefficients) {
                         
                         q_amp += cal_divdiff(coefficient, t, q, qubit, chi, omega, target);
                     }
                     //clear divdiff
                     divdiff_clear_up();
+                    std::cout << "post DIVDIFF" << std::endl;
 
+                    // std::cout << "will i get stuck?" << std::endl;
 
-                    // save q_amp to a file
-                    save_complex_to_file(q_amp, q_amp_filename);
+                    // // save q_amp to a file
+                    // save_complex_to_file(q_amp, q_amp_filename);
+                    
+                    // print energy_coefficient_amp
+                    std::cout << "Energy coefficient: " << energy_coefficient_amp.real() << " + " << energy_coefficient_amp.imag() << "i" << std::endl;
+                    //print q_amp
+                    std::cout << "Q: " << q << std::endl;
+                    std::cout << "Q_amp: " << q_amp.real().get_double() << " + " << q_amp.imag().get_double() << "i" << std::endl;
 
-                    q_amp = q_amp * energy_coefficient_amp;
+                    q_amp *= energy_coefficient_amp; 
 
                     // print the q amplitude
-                    std::cout << q << " Original q_amp: " << q_amp << std::endl;
+                    cout << q << " Original q_amp: " << endl;
+                    q_amp.real().print();
+                    cout << "+ ";
+                    q_amp.imag().print();
+                    cout << "i" << endl;
+
+                    cout << std::endl;
                     
                     //add to total amp
                     total_amp += q_amp;
+                    // total_amp.imag() += q_amp.imag();
                 }
 
-                // //print the Total amplitude
+                //print the Total amplitude
                 // std::cout << "Total amplitude: " << total_amp << std::endl << std::endl;
+                std::cout <<  "Total amp: "; 
+                total_amp.real().print();
+                std::cout << "+ ";
+                total_amp.imag().print();
+                std::cout << "i" << std::endl; 
+                std::cout << std::endl;
                 transition_amplitudes.push_back(total_amp);
             }
 
@@ -216,7 +240,12 @@ int main(int argc, char* argv[]) {
             std::cout << "Transition amplitudes: " << std::endl;
             int target_nums = 0;
             for (const auto& amp : transition_amplitudes) {
-                std::cout << target_nums << ": " << amp << std::endl;
+                std::cout << target_nums << ": "; 
+                amp.real().print();
+                std::cout << "+ ";
+                amp.imag().print();
+                std::cout << "i" << std::endl; 
+                std::cout << std::endl;
                 target_nums++;
             }
 
@@ -225,19 +254,22 @@ int main(int argc, char* argv[]) {
             
             // normalize transition amplitude 
             double norm = 0;
+            ExExFloat Ex_norm;
+            Ex_norm = 0;
             for (const auto& amp : transition_amplitudes) {
-                norm += std::norm(amp);
+                Ex_norm += amp.real() * amp.real() + amp.imag() * amp.imag();
             }
-            norm = sqrt(norm);
+            norm = sqrt(Ex_norm.get_double());
             for (auto& amp : transition_amplitudes) {
                 amp = amp / norm;
             }
-
-            if (qubit == 0){
-                all_transition_amplitudes_g.push_back(transition_amplitudes);
-            }else{
-                all_transition_amplitudes_e.push_back(transition_amplitudes);
-            }
+            
+            all_transition_amplitudes[qubit].push_back(transition_amplitudes);
+            // if (qubit == 0){
+            //     all_transition_amplitudes_g.push_back(transition_amplitudes);
+            // }else{
+            //     all_transition_amplitudes_e.push_back(transition_amplitudes);
+            // }
             // all_transition_amplitudes.push_back(transition_amplitudes);
 
             std::cout << std::endl << std::endl;
@@ -246,45 +278,45 @@ int main(int argc, char* argv[]) {
         }
 
 
-        if (qubit == 0){
-            all_transition_amplitudes = all_transition_amplitudes_g;
-        }else{
-            all_transition_amplitudes = all_transition_amplitudes_e;
-        }
+        // if (qubit == 0){
+        //     all_transition_amplitudes = all_transition_amplitudes_g;
+        // }else{
+        //     all_transition_amplitudes = all_transition_amplitudes_e;
+        // }
 
         //print all transition amplitudes with location in the vector
         std::cout << "All transition amplitudes: " << std::endl;
-        for (int i = 0; i < all_transition_amplitudes.size(); i++) {
+        for (int i = 0; i < all_transition_amplitudes[qubit].size(); i++) {
             std::cout << "Time: " << times[i] << std::endl;
-            for (int j = 0; j < all_transition_amplitudes[i].size(); j++) {
-                std::cout << "Target: " << j << " Amplitude: " << all_transition_amplitudes[i][j] << std::endl;
+            for (int j = 0; j < all_transition_amplitudes[qubit][i].size(); j++) {
+                std::cout << "Target: " << j << " Amplitude: ";
+                all_transition_amplitudes[qubit][i][j].real().print();
+                std::cout << "+ ";
+                all_transition_amplitudes[qubit][i][j].imag().print();
+                std::cout << "i" << std::endl;
+                cout << std::endl;
             }
         }
 
         // Assuming all_transition_amplitudes is a vector<vector<complex<double>>>
         nlohmann::json j;
-        for (const auto& vec : all_transition_amplitudes) {
+        for (const auto& vec : all_transition_amplitudes[qubit]) {
             nlohmann::json sub_j;
             for (const auto& c : vec) {
-                sub_j.push_back({{"real", c.real()}, {"imag", c.imag()}});
+                sub_j.push_back({{"real", c.real().get_double()}, {"imag", c.imag().get_double()}});
             }
             j.push_back(sub_j);
         }
+
+        std::string filename;
         if (qubit == 0){
-            std::string filename = "amplitudes_g_chg_amp" + std::to_string(amplitude) + "_q" + std::to_string(q_max) + ".json";
-            std::ofstream o(filename);
-            o << j << std::endl;
+            filename = "amplitudes_g_chg_amp" + std::to_string(amplitude) + "_q" + std::to_string(q_max) + ".json";
         }else{
-            std::string filename = "amplitudes_e_chg_amp" + std::to_string(amplitude) + "_q" + std::to_string(q_max) + ".json";
-            std::ofstream o(filename);
-            o << j << std::endl;
+            filename = "amplitudes_e_chg_amp" + std::to_string(amplitude) + "_q" + std::to_string(q_max) + ".json";
         }
+        std::ofstream o(filename);
+        o << j << std::endl;
     }
 
     return 0;
 }
-
-
-
-
-
--- show_trans_amp.py
+++ show_trans_amp.py
@@ -8,17 +8,34 @@ from warnings import warn
 from qutip import *
 
 
-# amp_g_json = [ 'amplitudes_g_chg_amp.json',  'amplitudes_e_chg_amp.json' ]
-# amp_g_json = [ 'amplitudes_e_chg_amp0.020000_q13.json',  'amplitudes_g_chg_amp0.020000_q13.json' ]
-# amp_g_json = [ 'amplitudes_e_chg_amp0.010000_q13.json',  'amplitudes_g_chg_amp0.010000_q13.json' ]
-# amp_g_json = [ 'amplitudes_e_chg_amp0.010000_q12.json',  'amplitudes_g_chg_amp0.010000_q12.json' ]
-amp_g_json = [ 'amplitudes_e_chg_amp0.007000_q12.json',  'amplitudes_g_chg_amp0.007000_q12.json' ]
+# amp_json = [ 'amplitudes_e_chg_amp0.020000_q13.json',  'amplitudes_g_chg_amp0.020000_q13.json' ]
+# amp_json = [ 'amplitudes_e_chg_amp0.010000_q13.json',  'amplitudes_g_chg_amp0.010000_q13.json' ]
+# amp_json = [ 'amplitudes_e_chg_amp0.100000_q12.json',  'amplitudes_g_chg_amp0.100000_q12.json' ]
+# amp_json = [ 'amplitudes_e_chg_amp0.100000_q11.json',  'amplitudes_g_chg_amp0.100000_q11.json' ]
+# amp_json = [ 'amplitudes_e_chg_amp0.200000_q12.json',  'amplitudes_g_chg_amp0.200000_q12.json' ]
+# amp_json = [ 'amplitudes_e_chg_amp0.020000_q12.json',  'amplitudes_g_chg_amp0.020000_q12.json' ]
+# amp_json = [ 'amplitudes_e_chg_amp0.005000_q8.json',  'amplitudes_g_chg_amp0.005000_q8.json' ]
+amp_json = [ 'amplitudes_e_chg_amp0.005000_q10.json',  'amplitudes_g_chg_amp0.005000_q10.json' ]
 
-# amp_g_json = [ 'amplitudes_g_chg_amp0.005000_q10.json' ]
-# amp_e_json = ['amplitudes_e_allQ.json' , 'amplitudes_e_chg_amp.json']
 
-# for file in amp_e_json:
-for file in amp_g_json:
+########################################################################
+# all_amp_g_json = []
+# all_amp_e_json = []
+
+# # create amp_json name files for different amplitude and different q
+# amplitudes = [ 0.001000, 0.002000, 0.005000, 0.010000, 0.020000, 0.050000, 0.060000, 0.100000, 0.200000]
+
+# for amp in amplitudes:
+#     for q  in range(8, 14):
+#         amp_e_json = 'amplitudes_e_chg_amp'+str(amp)+'q'+str(q)+'.json'
+#         amp_g_json = 'amplitudes_g_chg_amp'+str(amp)+'q'+str(q)+'.json'
+#         all_amp_e_json.append(amp_e_json)
+#         all_amp_g_json.append(amp_g_json)
+#########################################################################
+
+
+for file in amp_json:
+# for file in all_amp_g_json:
     # load transition amplitudes
     with open(file, 'r') as f:
         data = json.load(f)
@@ -26,10 +43,10 @@ for file in amp_g_json:
     all_transition_amplitudes = [[complex(d['real'], d['imag']) for d in sub_data] for sub_data in data]
 
     # print all_transition_amplitudes
-    print(all_transition_amplitudes)
+    # print(all_transition_amplitudes)
 
     N_plot = len(all_transition_amplitudes[0])
-    print(N_plot)
+    # print(N_plot)
 
     psi_t = []
     # create Qobj for each time frame in the transition amplitudes
@@ -39,11 +56,28 @@ for file in amp_g_json:
             psi += cn*basis(N_plot, n)
         psi_t.append(psi)
         
-    print(psi_t)
+    # print(psi_t)
 
     a = destroy(N_plot)
     alpha_t = expect(a, psi_t)
-    plt.figure()
-    plt.plot(alpha_t.real, alpha_t.imag)
+    # plt.figure()
+    # plt.plot(alpha_t.real, alpha_t.imag)
 
-    print(alpha_t[-1].real, alpha_t[-1].imag)
+    # # output all alpha_t to a file
+    # with open('alpha_t.json', 'w') as f:
+    #     json.dump(alpha_t, f)
+
+    # #print all alpha_t to txt file
+    # with open('alpha_t.txt', 'w') as f:
+    #     for item in alpha_t:
+    #         f.write("%s\n" % item)
+    #print last alpha_t
+    print()
+    print(file)
+    print("original_alpha_t")
+    print(alpha_t[-1])
+
+    # print("new alpha_t")
+    # print(alpha_t[-1])
+
+    # print()
